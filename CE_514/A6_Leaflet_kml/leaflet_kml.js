a155cc6a2a7885fcd1978b4c3af934f906b0b313
Ivy <stouti@byu.edu>
2025-02-08 17:38:05 -0700

    Code updatess

--- CE_514/A6_Leaflet_kml/leaflet_kml.js
+++ CE_514/A6_Leaflet_kml/leaflet_kml.js
@@ -1,7 +1,6 @@
 
 // Initialize the map and set its view to BYU's geographical coordinates and zoom level
-var map = L.map('map').setView([40.246331, -111.647653], 15);
-
+var map = L.map('map').setView([40.246331, -111.647653], 3);
 
 // Define tile layers
 var streetLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -18,7 +17,7 @@ streetLayer.addTo(map);
 // Add a marker to the map at BYU's coordinates
 L.marker([40.246331, -111.647653]).addTo(map)
     .bindPopup('Brigham Young University')
-    .openPopup();
+   // .openPopup();
 
 // Add layer control to switch between street and satellite views
 var baseLayers = {
@@ -26,29 +25,51 @@ var baseLayers = {
     "Satellite View": satelliteLayer
 };
 
-L.control.layers(baseLayers).addTo(map); //allows for the kml to be switched 
+L.control.layers(baseLayers).addTo(map); 
 
-// Initialize the KML layer and add it to the map when ready
+// Link to KML file downlaods for countries 
+// https://mapme.notion.site/402e1700c2c7496d88151b755d465428?v=814ee4b037114acc9789b9913a2b74d8
+
+//const kmlPath = 'KML_files/';
+
 const kmlUrls = [
-    'KML_import.kml',
-    'UtahCounty.kml'
+    // 'KML_files/Counties.kml',// For some reason it doesn't like Counties.kml
+   // kmlpath + 'USA.kml',
+    'KML_files/Utah.kml',
+    'KML_files/KML_import.kml',
+    'KML_files/Alabama.kml',
+    'KML_files/Alaska.kml',
+    'KML_files/Arizona.kml',
+    'KML_files/California.kml',
+    'KML_files/Florida.kml',
+    'KML_files/Georgia.kml',
+    'KML_files/Hawaii.kml',
+    'KML_files/Idaho.kml',
+    'KML_files/Maryland.kml',
+    'KML_files/Nevada.kml',
+    'KML_files/New York.kml',
+    'KML_files/Oklahoma.kml',   
+    'KML_files/South Dakota.kml',   
+    'KML_files/Texas.kml',
+    'KML_files/Utah.kml',   
+    'KML_files/Virginia.kml',
+    'KML_files/Washington.kml', 
+    'KML_files/Wyoming.kml',
+    'KML_files/District of Columbia.kml',
+    'KML_files/Canada.kml',
+    'KML_files/Mexico.kml',
+    'KML_files/Spain.kml',
+    'KML_files/Portugal.kml',
+    'KML_files/China.kml',
+    'KML_files/british-columbia.kml',
 ];
+
 
 kmlUrls.forEach(function(url) {
     omnivore.kml(url).on('ready', function() {
        //kmlLayer = this;
         this.addTo(map);
-        
-        this.eachLayer(function(layer) {
-            if (layer.feature.properties.name == 'Utah') {
-                layer.setStyle({
-                    color: 'red',
-                    fillColor: 'red',
-                    fillOpacity: 0.5
-                });
-            }
         }); 
     }).on('error', function(e) {
         console.error('Error loading KML:', e);
     });
-});

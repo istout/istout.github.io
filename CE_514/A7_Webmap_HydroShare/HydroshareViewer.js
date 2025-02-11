2a4b7f3b0660850bf8e0236262cdc34837b2e9b3
istout <87056817+istout@users.noreply.github.com>
2025-02-11 08:55:13 -0700

    updates

--- CE_514/A7_Webmap_HydroShare/HydroshareViewer.js
+++ CE_514/A7_Webmap_HydroShare/HydroshareViewer.js
@@ -1,46 +1,47 @@
 
 var map = L.map('map').setView([41.8, -111.48], 9); //Centered on Logan
 
- L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+}).addTo(map);
 
-        const layer1 =  L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-a51c74b3a8734324a53bb2c29ae9df3b/wms', { 
+const layer1 = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-a51c74b3a8734324a53bb2c29ae9df3b/wms', { 
+    layers: 'a51c74b3a8734324a53bb2c29ae9df3b:Loganw', 
+format: 'image/png',
+transparent: true,
+attribution: 'Hydroshare GeoServer'
+});
+
+layer1.addTo(map);
+
+const layer2 =  L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-a51c74b3a8734324a53bb2c29ae9df3b/wms', { 
       	  	layers: 'a51c74b3a8734324a53bb2c29ae9df3b:Logannet', 
             format: 'image/png',
             transparent: true,
             attribution: 'Hydroshare GeoServer'
         })
-       layer1.addTo(map); 
+layer2.addTo(map); 
       
-const layer2 = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-a51c74b3a8734324a53bb2c29ae9df3b/wms', { 
-      	  	layers: 'a51c74b3a8734324a53bb2c29ae9df3b:Loganw', 
-            format: 'image/png',
-            transparent: true,
-            attribution: 'Hydroshare GeoServer'
-        });
-        
-        layer2.addTo(map);
-        
+
           
-        function toggleLayer1() {
-            if (document.getElementById("layer1checkbox").checked) {
-                layer1.addTo(map);
-            } else {
-                map.removeLayer(layer1);
-            }
-        }
+function toggleLayer1() {
+    if (document.getElementById("layer1checkbox").checked) {
+      layer1.addTo(map);
 
-        // Toggle Mom's Ancestry Layers
-        function toggleLayer2() {
-            if (document.getElementById("layer2checkbox").checked) {
-                layer2.addTo(map);
+  } else {
+      map.removeLayer(layer1);
+  }
+}
 
-            } else {
-                map.removeLayer(layer2);
-        
-            }
-        }
-   
+function toggleLayer2() {
+    if (document.getElementById("layer2checkbox").checked) {
+        layer2.addTo(map);
+
+    } else {
+        map.removeLayer(layer2);
+
+    }
+}
+
         
